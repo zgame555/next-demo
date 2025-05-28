ad163795f00b76789da24f1ef46fbcbc56badbad
zgame <premwit.min@gmail.com>
2025-05-28 22:34:43 +0700

    add

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,9 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
+        <div className="p-2 bg-sky-500 rounded-md text-white text-center font-bold text-lg">
+          {process.env.NEXT_PUBLIC_NAME || 'No Name'}
+        </div>
         <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
           <DomainName />
         </code>
