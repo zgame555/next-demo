ed75e57abaa7bd5622fe0ca83fca571606f67720
zgame <premwit.min@gmail.com>
2025-05-28 22:45:26 +0700

    add

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  standalone: true,
+  reactStrictMode: false,
+}
 
-export default nextConfig;
+export default nextConfig
